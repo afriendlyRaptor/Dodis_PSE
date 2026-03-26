19f08c4538e207c010b13ec2602c8b49651d2529
Meier Paul <paul.meier@students.unibe.ch>
2026-03-26 10:16:18 +0100

    loaded correct python modules, increaced memory allocation

--- src/ubelix_job_script.sh
+++ src/ubelix_job_script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1GB
+#SBATCH --mem-per-cpu=64GB
 #SBATCH --account=gratis
 #SBATCH --qos=job_gratis
 
 # Put your code below this line
 module load Workspace_Home
+module load Python/3.12.3-GCCcore-13.3.0
 source setup.sh
 source start.sh
