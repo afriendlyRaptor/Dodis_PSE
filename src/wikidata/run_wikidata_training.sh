96c4f713c7058567f2b645f6cbba718fe113253c
robinisaghost <robin.vandenhoek@students.unibe.ch>
2026-04-27 14:27:17 +0200

    Integrate setup.sh into training scripts, fix CUDA version

--- src/wikidata/run_wikidata_training.sh
+++ src/wikidata/run_wikidata_training.sh
@@ -10,7 +10,7 @@
 
 #SBATCH --gres=gpu:rtx4090:1
 ##SBATCH --gres=gpu:rtx3090:1
-#SBATCH --job-name=DodisWiki
+#SBATCH --job-name=WikiNEL
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
@@ -21,16 +21,36 @@
 #SBATCH --output=job_logs/output_%j.out
 #SBATCH --error=job_logs/output_%j.err
 
+set -e
+
 module purge
 module load Workspace_Home
 module load Python/3.12.3-GCCcore-13.3.0
-module load CUDA/12.1.1
+module load CUDA/12.6.0
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-source src/setup.sh
+if [ ! -d "venv" ]; then
+    echo "Kein venv gefunden. Erstelle neues venv..."
+    python3 -m venv venv
+    source venv/bin/activate
 
-source venv/bin/activate
+    python -m pip install --upgrade pip setuptools wheel
+    pip install torch==2.5.1+cu121 --index-url https://download.pytorch.org/whl/cu121
+    pip install "cupy-cuda12x<13"
+    pip install spacy spacy-transformers
+    pip install -r src/requirements.txt
+
+    python -m spacy download de_dep_news_trf
+    python -m spacy download de_core_news_sm
+    python -m spacy download de_core_news_lg
+
+    echo "Setup abgeschlossen!"
+else
+    echo "venv existiert bereits. Überspringe Installation."
+    source venv/bin/activate
+fi
+
 python3 -c "import torch; print('CUDA Available:', torch.cuda.is_available()); print('CUDA Device Count:', torch.cuda.device_count()); print('CUDA Version:', torch.version.cuda)"
 nvidia-smi
 
